45781d92cc626880154d8c02f95b3366194ad44e
LOKESH REDDI <reddi.lokesh@gmail.com>
2022-12-14 13:01:26 -0500

    commit changes for title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from "next";
 import Game from "../components/Game/Game";
+import Head from 'next/head'
+
 
 const Home: NextPage = () => {
 
   return (
-    <Game />
+    <>
+      <Head>
+        <title>Tollywoodle</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Game />
+    </>
   );
 };
 
